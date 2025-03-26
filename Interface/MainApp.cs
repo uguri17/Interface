1f4a6aa781a40819a35cde893fb893052e7243a4
RND-HW3 <RND-HW3@RND-HW3>
2025-03-26 13:16:21 +0900

    초기 커밋.

---
+++ Interface/MainApp.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace Interface
+{
+    interface ILogger
+    {
+        void WriteLog(string message);
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        public void WriteLog(string message)
+        {
+            Console.WriteLine("{0} {1}", DateTime.Now.ToLocalTime(), message);
+        }
+    }
+
+    class FileLogger : ILogger
+    {
+        private StreamWriter writer;
+
+        public FileLogger(string path)
+        {
+            writer = File.CreateText(path);
+            writer.AutoFlush = true;
+        }
+
+        public void WriteLog(string message)
+        {
+            Console.WriteLine("{0} {1}", DateTime.Now.ToShortTimeString(), message);
+        }
+    }
+
+    class ClimateMonitor
+    {
+        private ILogger logger;
+
+        public ClimateMonitor(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public void start()
+        {
+            while (true)
+            {
+                Console.Write("온도 입력: ");
+
+                string temperature = Console.ReadLine();
+                if (temperature == "")
+                {
+                    break;
+                }
+
+                logger.WriteLog($"현재온도: {temperature}");
+            }
+        }
+    }
+    
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            ClimateMonitor monitor = new ClimateMonitor(new ConsoleLogger());
+
+            monitor.start();
+        }
+    }
+}
